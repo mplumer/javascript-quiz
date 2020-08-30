3a0770cf2d168a154b7dcd0ce7f8463549ac05be
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-08-29 20:31:05 -0400

    create Q&A and make choices clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-var startBtn = document.querySelector("#start");
-var answerBtn = document.querySelector(".btn")
-var quizObj = {
+var mainEl = document.querySelector('#main');
+var newCard = document.querySelector('#newCard');
+var startButton = document.querySelector('#start');
+var answerButton = document.querySelectorAll(".answer-btn");
+var button = document.getElementsByTagName('button');
+
+var question1 = "The condition in an if/else statement is enclosed with ____.</h2>";
+var answer1 = ["quotes", "curly brackets", "parentheses", "square brackets"];
+var question2 = "Commonly used data types DO NOT include:";
+var answer2 = ["strings", "booleans", "alerts", "numbers"];
+var question3 = "A very useful tool used during development and debugging for printing content to the debugger is:";
+var answer3 = ["JavaScript", "Terminal/Bash", "for loops", "console.log"];
+var question4 = "String values must be enclodsed within __________ when being assigned to variables.";
+var answer4 = ["commas", "curly brackets", "quotes", "parentheses"];
+var question5 = "Arrays in JavaScript can be used to store __________";
+var answer5 = ["numbers and strings", "other arrays", "booleans", "all of the above"];
+
+/*var quizObj = {
 
 }
 
@@ -18,66 +33,77 @@ var countDown = function () {
         };
     }, 1000);
     startGame();
-};
+};*/
 
 
 
 var startQuiz = function (event) {
     event.preventDefault();
-    var startCard = document.getElementById("#startCard");
-    
+    var newCard = document.getElementById("#newCard");
+    //newCard.remove();
     createCard()
 
-}
+};
 
-var createCard = function (quizObj) {
+var createCard = function () {
     // create question 
     var questionEl = document.createElement("article");
     questionEl.className = "question";
     questionEl.innerHTML = "<h2>Commonly used data types DO NOT include:</h2>";
 
-    // create 
-    var answer1El = document.createElement("btn");
+    // create answers
+    var answer1El = document.createElement("button");
     answer1El.textContent = "1. alerts";
     answer1El.id = "answer1";
-    answer1El.className = "btn";
-    var answer2El = document.createElement("btn");
+    answer1El.className = "answer-btn";
+    var answer2El = document.createElement("button");
     answer2El.textContent = "2. booleans";
     answer2El.id = "answer2";
-    answer2El.className = "btn";
-    var answer3El = document.createElement("btn");
+    answer2El.className = "answer-btn";
+    var answer3El = document.createElement("button");
     answer3El.textContent = "3. numbers";
     answer3El.id = "answer3";
-    answer3El.className = "btn";
-    var answer4El = document.createElement("btn");
+    answer3El.className = "answer-btn";
+    var answer4El = document.createElement("button");
     answer4El.textContent = "4. strings";
     answer4El.id = "answer4";
-    answer4El.className = "btn";
+    answer4El.className = "answer-btn";
 
     questionEl.appendChild(answer1El);
     questionEl.appendChild(answer2El);
     questionEl.appendChild(answer3El);
     questionEl.appendChild(answer4El);
 
-    var mainEl = document.getElementById("#main");
     mainEl.appendChild(questionEl);
 
     console.log(mainEl);
-    // console.log(answer1El)
-}
+    console.log(choice1El)
+};
+
+
+var checkAnswer = function () {
+
+};
 
 var gameOver = function () {
 
-}
+};
 
 var highScore = function () {
 
-}
+};
 
 
 
 
-startBtn.addEventListener("click", startQuiz);
-// pageContentEl.addEventListener("click", taskbtnHandler);
+startButton.addEventListener("click", startQuiz);
 
-// startQuiz()
+document.querySelector("#main").addEventListener('click', function (e) {
+    if (e.target.classList.contains('answer-btn')) {
+        alert('clicked');
+    }
+})
+// choiceButton.addEventListener("click", checkAnswer);
+// pageContentEl.addEventListener("click", taskButtonHandler);
+
+//startQuiz(e)
